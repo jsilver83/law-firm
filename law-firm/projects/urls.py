50cce7290b33cd2bc9bfd7be9be2124289588532
Abdullah Joud <joud@kfupm.edu.sa>
2018-01-18 00:48:25 +0300

    rewrote urls form of the routes

--- law-firm/projects/urls.py
+++ law-firm/projects/urls.py
@@ -7,22 +7,22 @@ urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
 
     url(r'^listing/(?P<p_type>\w+)/$', views.ProjectListing.as_view(), name='case_listing'),
-    url(r'^project/(?P<p_type>\w+)/$', views.NewProjectView.as_view(), name='new_project'),
+    url(r'^new-project/(?P<p_type>\w+)/$', views.NewProjectView.as_view(), name='new_project'),
     url(r'^update_project/(?P<pk>\d+)/$', views.UpdateProjectView.as_view(), name='update_project'),
-    url(r'^project/(?P<pk>\d+)/update/$', views.NewUpdateView.as_view(), name='new_update'),
+    url(r'^project/(?P<pk>\d+)/new-update/$', views.NewUpdateView.as_view(), name='new_update'),
     url(r'^project/(?P<pk>\d+)/request_fund/$', views.NewFundRequestView.as_view(), name='new_request_fund'),
     url(r'^project/(?P<pk>\d+)/reminder/$', views.NewReminderView.as_view(), name='new_project_reminder'),
 
     url(r'^clients/$', views.ClientListing.as_view(), name='clients'),
-    url(r'^client/$', views.NewClientView.as_view(), name='new_client'),
+    url(r'^new-client/$', views.NewClientView.as_view(), name='new_client'),
     url(r'^client/(?P<pk>\d+)/$', views.ClientView.as_view(), name='update_client'),
 
     url(r'^organizations/$', views.OrganizationListing.as_view(), name='organizations'),
-    url(r'^organization/$', views.NewOrganizationView.as_view(), name='new_organization'),
+    url(r'^new-organization/$', views.NewOrganizationView.as_view(), name='new_organization'),
     url(r'^organization/(?P<pk>\d+)/$', views.OrganizationView.as_view(), name='update_organization'),
 
     url(r'^employees/$', views.EmployeesListing.as_view(), name='employees'),
-    url(r'^employee/$', views.NewEmployeeView.as_view(), name='new_employee'),
+    url(r'^new-employee/$', views.NewEmployeeView.as_view(), name='new_employee'),
     url(r'^employee/(?P<pk>\d+)/$', views.EmployeeView.as_view(), name='update_employee'),
     url(r'^new-user/$', views.NewUserView.as_view(), name='new-user'),
     url(r'^user/(?P<pk>\d+)/$', views.ChangeUserView.as_view(), name='change-user'),
@@ -33,9 +33,5 @@ urlpatterns = [
     url(r'^nationality-autocomplete/$',
         auto_complete_views.NationalityAutocomplete.as_view(),
         name='nationality-autocomplete', ),
-
-    # url(r'$', al.CreateView.as_view(
-    #     model=Client, form_class=ClientForm),
-    #     name='client_add_another_model_create'),
 ]
 

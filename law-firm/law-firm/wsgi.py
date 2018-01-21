ecc46db295e6c0a7e6b079a7a287bcd782777518
Abdullah Joud <joud@kfupm.edu.sa>
2018-01-21 17:56:25 +0300

    added code to create initial groups and this code will execute once

--- law-firm/law-firm/wsgi.py
+++ law-firm/law-firm/wsgi.py
@@ -9,8 +9,20 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+from django.contrib.auth.models import Group
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gentella.settings")
 
 application = get_wsgi_application()
+
+####################################
+# Creating initial user groups     #
+# This code will execute once only #
+####################################
+created = False
+admins_group, created = Group.objects.get_or_create(name='Admins')
+lawyers_group, created = Group.objects.get_or_create(name='Lawyers')
+archive_group, created = Group.objects.get_or_create(name='Archive')
+accounting_group, created = Group.objects.get_or_create(name='Accounting')

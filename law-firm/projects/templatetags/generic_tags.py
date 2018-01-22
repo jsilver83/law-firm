47efeecc1503b9a03979e44a403205b1f858d680
Abdullah Joud <joud@kfupm.edu.sa>
2018-01-23 00:32:32 +0300

    made sidebar menu dynamic based on logged in user groups

---
+++ law-firm/projects/templatetags/generic_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('sidebar.html', takes_context=True)
+def sidebar_navigation_menu(context):
+    user = context['request'].user
+
+    admin = user.groups.filter(name__in=['Admins']).exists() or user.is_superuser
+    lawyer = user.groups.filter(name__in=['Admins', 'Lawyers']).exists() or user.is_superuser
+    accounting = user.groups.filter(name__in=['Admins', 'Accounting']).exists() or user.is_superuser
+    archive = user.groups.filter(name__in=['Admins', 'Archive']).exists() or user.is_superuser
+
+    return {
+        'user': user,
+        'admin': admin,
+        'lawyer': lawyer,
+        'accounting': accounting,
+        'archive': archive,
+    }

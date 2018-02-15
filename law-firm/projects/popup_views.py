07f5d3982d48d77730a22d5163a41fb910e281b1
Abdullah Joud <joud@kfupm.edu.sa>
2018-02-15 12:14:00 +0300

    fixed permission for inline add-another

--- law-firm/projects/popup_views.py
+++ law-firm/projects/popup_views.py
@@ -1,35 +1,36 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, UpdateView
 from django_addanother.views import CreatePopupMixin, UpdatePopupMixin
 
-from .views import NewOrganizationView, BaseFormView
+from .views import NewOrganizationView, BaseFormView, BaseAdminView, BaseLawyerView
 from .forms import MyUserCreationForm, MyUserChangeForm, OrganizationForm, NationalityForm, ClientForm, CourtForm
 
 
-class NewUserView(CreatePopupMixin, CreateView):
+class NewUserView(BaseAdminView, CreatePopupMixin, CreateView):
     form_class = MyUserCreationForm
     template_name = 'projects/plain-form.html'
 
 
-class ChangeUserView(UpdatePopupMixin, UpdateView):
+class ChangeUserView(BaseAdminView, UpdatePopupMixin, UpdateView):
     form_class = MyUserChangeForm
     template_name = 'projects/plain-form.html'
 
 
-class NewClientPopupView(CreatePopupMixin, BaseFormView, CreateView):
+class NewClientPopupView(LoginRequiredMixin, CreatePopupMixin, BaseFormView, CreateView):
     form_class = ClientForm
     template_name = 'projects/plain-form.html'
 
 
-class NewOrganizationPopupView(CreatePopupMixin, BaseFormView, CreateView):
+class NewOrganizationPopupView(LoginRequiredMixin, CreatePopupMixin, BaseFormView, CreateView):
     form_class = OrganizationForm
     template_name = 'projects/plain-form.html'
 
 
-class NewCourtPopupView(CreatePopupMixin, BaseFormView, CreateView):
+class NewCourtPopupView(BaseLawyerView, CreatePopupMixin, BaseFormView, CreateView):
     form_class = CourtForm
     template_name = 'projects/plain-form.html'
 
 
-class NewNationalityPopupView(CreatePopupMixin, BaseFormView, CreateView):
+class NewNationalityPopupView(LoginRequiredMixin, CreatePopupMixin, BaseFormView, CreateView):
     form_class = NationalityForm
     template_name = 'projects/plain-form.html'
